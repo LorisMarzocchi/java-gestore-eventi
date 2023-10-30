0963d66b5df825cb2707eb1b53700c5589012869
LorisMarzocchi <ml77182a@gmail.com>
2023-10-30 09:37:43 +0100

    First commit

--- src/org/lessons/java/Evento.java
+++ src/org/lessons/java/Evento.java
@@ -1,2 +1,35 @@
-package org.lessons.java;public class Evento {
+package org.lessons.java;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+    private String nome;
+    private LocalDate data;
+    private int postiTotali = 0;
+    private int postiPrenotati;
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
